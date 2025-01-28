a519eb42d95d9c95a8a5bd85bcdf91eaab22c128
HasanRabieehanjani <hasanrabieehanjani.78@gmail.com>
2025-01-28 14:28:25 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export const metadata = {
+  title: 'Homepage',
+}
+
+const Homepage = () => {
+  return (<Button>test</Button>);
+}
+
+export default Homepage;
